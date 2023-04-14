20ad44694d5215b661324dee6c040bb2650ee02c
Said-belaroussi <said.belaroussi@ulb.be>
2023-04-14 15:38:56 +0200

    added "N" NOP move + modified GA to be closer to the research paper

--- src/genetic/simple_ga.py
+++ src/genetic/simple_ga.py
@@ -28,17 +28,38 @@ class GeneticAlgorithm:
             random.choices([*self.cube.perms.keys()], k=self.length_individual)
             for _ in range(self.nb_individuals)
         ]
-
         for generation_count in range(self.nb_generations):
             print("<============================>")
             print(f"Generation {generation_count}")
-            gen = copy.deepcopy(generation)
-            generation = self.next_generation(generation)
+            mate_pool = []
+            current_generation = copy.deepcopy(generation)
+            # generation = current_generation[5:95]
+            # generation.extend(current_generation[40:50])
+            evaluated = [self.evaluate(individual) if self.evaluate(individual) != 0 else 100 for individual in
+                         current_generation]
+            print(evaluated)
+            evaluated = [self.evaluate(individual) if self.evaluate(individual) != 0 else 100 for individual in
+                         generation]
+            print(evaluated)
+            # Selection
+            generation = self.selection(generation)
+            mate_pool.extend(self.crossover(generation))
 
             for individual in generation:
-                self.mutate(individual)
+                if random.random() < self.mutation_rate:
+                    individual = self.mutate(individual)
+                    mutated = self.mutate(individual)
+                    if self.evaluate(individual) != self.evaluate(mutated):
+                        mate_pool.append(mutated)
+            mate_pool.extend(self.select_best(current_generation, int(len(current_generation)/10)))
+            print(mate_pool)
 
-            print(len(generation))
+            generation = self.select_best(mate_pool, len(generation))
+            check_duplicates_generation = []
+            for i in generation:
+                if i not in check_duplicates_generation:
+                    check_duplicates_generation.append(i)
+            print("generation size: ", len(generation), "\n#non-duplicates: ", len(check_duplicates_generation))
 
             best_score = self.evaluate(generation[0])
             best_ind = generation[0]
@@ -48,41 +69,80 @@ class GeneticAlgorithm:
 
             print(f"best : {best_score}")
             print(best_ind)
-            print(cube.permute(best_ind))
+            if best_score == 0:
+                break
+            #print(cube.permute(best_ind))
 
             # print(min([self.evaluate(individual) for individual in generation]))
 
-    def mutate(self, individual):
-        if random.random() < self.mutation_rate:
-            index = random.randint(0, len(individual) - 1)
 
-            possibilities = [*self.cube.perms.keys()]
-            gene_value = individual[index]
-            possibilities.remove(gene_value)
+    def select_best(self, generation, trunc_value):
+        new_generation = []
+        # Evaluate each individual
+        evaluated = [self.evaluate(individual) for individual in generation]
+        list_evaluated_generation = []
+        # Sorting based on evaluation
+        for i in range(len(generation)):
+            list_evaluated_generation.append((evaluated[i], generation[i]))
+
+        list_evaluated_generation.sort()
+
+        try:
+            for i in list_evaluated_generation[:trunc_value]:
+                new_generation.append(i[1])
+        except:
+            print("trunc_value is too high")
+        return new_generation
+
+    def select_worst(self, generation, trunc_value):
+        new_generation = []
+        # Evaluate each individual
+        evaluated = [self.evaluate(individual) for individual in generation]
+        list_evaluated_generation = []
+        # Sorting based on evaluation
+        for i in range(len(generation)):
+            list_evaluated_generation.append((evaluated[i], generation[i]))
+
+        list_evaluated_generation.sort(reverse=True)
+
+        try:
+            for i in list_evaluated_generation[:trunc_value]:
+                new_generation.append(i[1])
+        except:
+            print("trunc_value is too high")
+        return new_generation
+
+    def mutate(self, individual):
+        index = random.randint(0, len(individual) - 1)
+
+        possibilities = [*self.cube.perms.keys()]
+        for i in range(5):
+            possibilities.append("N")
+        gene_value = individual[index]
+        possibilities.remove(gene_value)
+        try:
+            possibilities.remove(invert_moves([gene_value])[0])
+        except ValueError:
+            pass
+        if index > 0:
             try:
-                possibilities.remove(invert_moves([gene_value])[0])
+                possibilities.remove(individual[index - 1])
             except ValueError:
                 pass
-            if index > 0:
-                try:
-                    possibilities.remove(individual[index - 1])
-                except ValueError:
-                    pass
-            if index < len(individual) - 1:
-                try:
-                    possibilities.remove(individual[index + 1])
-                except ValueError:
-                    pass
+        if index < len(individual) - 1:
+            try:
+                possibilities.remove(individual[index + 1])
+            except ValueError:
+                pass
+        mutated = individual.copy()
+        random_choice = random.choice(possibilities)
+        mutated[index] = random_choice
 
-            before_mutation = individual.copy()
-            random_choice = random.choice(possibilities)
-            before_mutation[index] = random_choice
+        # if self.evaluate(individual) > self.evaluate(mutated):
+        #     individual[index] = random_choice
 
-            # TODO: Maybe it's better to do mutation anyway cuz we often get stuck with good but not good enough solutions
-            if self.evaluate(individual) > self.evaluate(before_mutation):
-                individual[index] = random_choice
+        return mutated
 
-        return individual
     def next_generation(self, generation):
         """
         The chromosomes of the current population are selected using roulette wheel method with probability =80%
@@ -90,12 +150,13 @@ class GeneticAlgorithm:
         """
 
         # Selection
+        roulette_wheel = self.selection(generation)
         selected_individuals = self.selection_elitist(generation)
 
         # Crossover
-        new_generation = self.crossover(selected_individuals)
+        crossed_generation = self.crossover(roulette_wheel)
 
-        return new_generation
+        return crossed_generation
 
     def selection(self, generation):
 
@@ -149,18 +210,24 @@ class GeneticAlgorithm:
         Crossover two individuals
         """
         crossover_point = random.randint(0, self.length_individual - 1)
-
+        acceptable_children = []
         x = [
             individual1[:crossover_point] + individual2[crossover_point:],
             individual2[:crossover_point] + individual1[crossover_point:],
         ]
 
-        if self.evaluate(x[0]) < self.evaluate(individual1):
-            individual1 = x[0]
-        if self.evaluate(x[1]) < self.evaluate(individual2):
-            individual2 = x[1]
+        # if self.evaluate(x[0]) < self.evaluate(individual1):
+        #     individual1 = x[0]
+        # if self.evaluate(x[1]) < self.evaluate(individual2):
+        #     individual2 = x[1]
+        #if self.evaluate(x[0]) <= self.evaluate(individual1):
+        acceptable_children.append(x[0])
+        #if self.evaluate(x[1]) <= self.evaluate(individual2):
+        acceptable_children.append(x[1])
 
-        return [individual1, individual2]
+        # return [individual1, individual2]
+        return acceptable_children
+
 
     def evaluate(self, individual):
         cube = self.cube.permute(individual)
@@ -168,10 +235,30 @@ class GeneticAlgorithm:
         return self.evaluation_function(Cube(cube.n, cube))
 
 
+    # def evaluate(self, perms):
+    #     new_cube = list(self.cube.cube)
+    #     min = 24
+    #     for perm in perms:
+    #         save = list(new_cube)
+    #         mvt = self.cube.perms[perm]
+    #         for t in mvt:
+    #             u = (t[-1],) + t
+    #             for i, v in enumerate(t):
+    #                 new_cube[v] = save[u[i]]
+    #         cubee = Cube(self.cube.n, new_cube)
+    #         value = self.evaluation_function(Cube(cubee.n, cubee))
+    #         if value < min:
+    #             min = value
+    #     return min
+
+
+
 if __name__ == "__main__":
     cube = Cube(2)
     # print(cube.scramble(8))
     cube = cube.permute(["R'", 'D', "F'", 'U', "L'", 'D', 'B', 'L'])
+    # cube = cube.permute(["R'", 'D', "F'", 'U', "L'", 'D', 'B'])
+    # cube = cube.permute(["R'", 'D', "F'", 'U', "L'"])
     GeneticAlgorithm(
-        100, 200, 15, 0.5, cube, entropy_based_score_evaluation_function
+        100, 500, 30, 0.5, cube, entropy_based_score_evaluation_function
     ).run()
