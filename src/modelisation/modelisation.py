304a693807d0cacce20696ea92c725fbe2dcc512
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-04-16 19:27:36 +0200

    Adding machine learning

--- src/modelisation/modelisation.py
+++ src/modelisation/modelisation.py
@@ -86,7 +86,7 @@ class Cube:
 
         return shuffle
 
-    def permute(self, perms: list[str]):
+    def permute(self, perms: List[str]):
         """
         Apply a permutation to the current cube state
 
