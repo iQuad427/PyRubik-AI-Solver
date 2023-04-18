8c8f11ac3defb24682736a9ea89e68896e18c322
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-04-18 19:49:17 +0200

    update search

--- bin/search.py
+++ bin/search.py
@@ -1,81 +1,115 @@
 import math
-import random
-
-import matplotlib.pyplot as plt
 
 from src.modelisation.modelisation import Cube
 from src.neural_network.ai_heuristic.super import deep_learning_evaluate_function
 from src.search.evaluation.look_up.functions.distances import (
+    simple_distances_total_independent_moves_3x3,
     simple_distances_total_independent_moves_all_3x3,
 )
-from src.search.evaluation.membership import face_color_membership_evaluation_function
 from src.search.models.game_state import GameState
 from src.search.uninformed.depth import DepthFirstSearchEngine
 
-# Define the number of cubes to solve
-NUM_CUBES = 20
-# Define the maximum depth for the search
-MAX_DEPTH = 4
 
-MAX_SCRAMBLES = 20
-
-
-# Initialize variables to keep track of the number of solved cubes
-num_solved = [0] * (MAX_SCRAMBLES - 4)
-num_trials = [0] * (MAX_SCRAMBLES - 4)
-
-for i in range(NUM_CUBES):
-    # Initialize the Rubik's Cube and scramble it between 5 and 15 moves
-    cube = Cube(3)
-    num_scrambles = random.randint(5, MAX_SCRAMBLES)
-    num_trials[num_scrambles - 5] += 1
-    cube.scramble(num_scrambles)
-
-    # Initialize the search engine
-    eval_func_rotations = [
-        deep_learning_evaluate_function,
-    ]
-    x = 0
+def solve_using_standard_heuristic(cube):
     previous_best_score = math.inf
-    depth = 2
 
-    print(
-        "Initial evaluation: " + str(deep_learning_evaluate_function(GameState(cube)))
-    )
-
-    # Search for a solution
     while True:
-        eval_func = eval_func_rotations[x % len(eval_func_rotations)]
-        engine = DepthFirstSearchEngine(GameState(cube), eval_func, depth)
-        result = engine.run()
+        engine = DepthFirstSearchEngine(
+            GameState(cube), simple_distances_total_independent_moves_3x3, 4
+        )
+
+        engine.run()
 
         # If a solution is found, update the number of solved cubes for the corresponding scramble level
         best_score = min(engine.best_scores)
-        if best_score == 1:
-            num_solved[num_scrambles - 5] += 1
-            break
-
-        # Adjust the depth of the search if there is no improvement
-        if previous_best_score == best_score:
-            depth += 1
-            if depth > MAX_DEPTH:
-                break
-        else:
-            depth = 2
-            previous_best_score = best_score
 
         cube = engine.best_founds[engine.best_scores.index(best_score)].cube
-        x += 1
+        print("S: Best score: " + str(best_score))
 
-# Display the number of solved cubes per scramble level in percentage
-plt.bar(
-    [i for i in range(5, MAX_SCRAMBLES)],
-    [
-        (num_solved[i] / num_trials[i]) * 100 if num_trials[i] else 1
-        for i in range(MAX_SCRAMBLES - 5)
-    ],
-    color="blue",
-)
-plt.xlabel("Number of scrambles")
-plt.ylabel("Percentage of solved cubes")
-plt.show()
+        if best_score < previous_best_score:
+            print(
+                "S: New best score: " + str(best_score),
+                "Previous: " + str(previous_best_score),
+            )
+            previous_best_score = best_score
+        else:
+            return cube, previous_best_score
+
+        if best_score == 0:
+            print("Solved using standard heuristic")
+
+
+def solve_using_total_standard_heuristic(cube):
+    previous_best_score = math.inf
+
+    while True:
+        engine = DepthFirstSearchEngine(
+            GameState(cube), simple_distances_total_independent_moves_all_3x3, 4
+        )
+
+        engine.run()
+
+        # If a solution is found, update the number of solved cubes for the corresponding scramble level
+        best_score = min(engine.best_scores)
+
+        cube = engine.best_founds[engine.best_scores.index(best_score)].cube
+        print("R: Best score: " + str(best_score))
+
+        if best_score < previous_best_score:
+            print(
+                "R: New best score: " + str(best_score),
+                "Previous: " + str(previous_best_score),
+            )
+            previous_best_score = best_score
+        else:
+            return cube, previous_best_score
+
+        if best_score == 8:
+            print(cube)
+            print("Solved using standard heuristic")
+
+
+def solve_using_deep_and_standard_when_blocked(cube):
+    previous_best_score = math.inf
+    previous_heuristic_score = math.inf
+    while True:
+        engine = DepthFirstSearchEngine(
+            GameState(cube), deep_learning_evaluate_function, 3
+        )
+        engine.run()
+
+        # If a solution is found, update the number of solved cubes for the corresponding scramble level
+        best_score = min(engine.best_scores)
+
+        cube = engine.best_founds[engine.best_scores.index(best_score)].cube
+
+        if best_score < previous_best_score:
+            print("New best score: " + str(best_score))
+            previous_best_score = best_score
+        else:
+            print("Launching standard heuristic")
+            cube, heur_score = solve_using_standard_heuristic(cube)
+            if heur_score < previous_heuristic_score:
+                previous_heuristic_score = heur_score
+            else:
+                cube, heur_score = solve_using_total_standard_heuristic(cube)
+                previous_heuristic_score = math.inf
+
+            previous_best_score = math.inf
+
+        if best_score < 50:
+            print("Solved")
+            break
+
+    print("Final cube:")
+    print(cube)
+
+
+cube = Cube(3)
+cube.scramble(15)
+print("Initial cube:")
+print(cube)
+# Search for a solution
+beginner_moves = ["U", "D", "R", "L", "F", "B"]
+
+solve_using_deep_and_standard_when_blocked(cube)
