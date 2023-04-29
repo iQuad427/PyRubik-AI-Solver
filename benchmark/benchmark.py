748b18451e5d17336679f94a519cf49129466654
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-04-29 18:04:49 +0200

    Improving benchmarking

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,6 +1,6 @@
-import threading
+import multiprocessing
 from datetime import datetime
-from queue import Queue
+import time
 from typing import List
 
 from matplotlib import pyplot as plt
@@ -35,65 +35,82 @@ def run_game_engine(game_engine, evaluation_function, best_score_list, cube):
     instantiated_engine.run()
 
 
-time_limit = 1  # Time limit in seconds
-
-results = {}
-
-x = 0
-
-for game_engine in GAME_ENGINES:
-    for name, evaluation_function in EVALUATION_FUNCTIONS.items():
+def compute_score_evolution_for_game_engine_and_evaluation_functions(engine_eval_runs):
+    time_limit = 120  # Time limit in seconds
+    results = []
+    processes = []
+    names = []
+    a = 0
+    for game_engine, evaluation_function in engine_eval_runs:
+        a += 1
+        if a > 20:
+            break
         print(
-            "Using game engine", game_engine.__name__, "and evaluation function", name
+            "Using game engine",
+            game_engine.__name__,
+            "and evaluation function",
+            evaluation_function.__name__,
         )
 
         cube = Cube(3)
-        cube.scramble(10)
+        cube.scramble(20)
 
-        best_score_list = []
-        game_engine_thread = threading.Thread(
+        manager = multiprocessing.Manager()
+        best_score_list = manager.list()
+
+        game_engine_process = multiprocessing.Process(
             target=run_game_engine,
             args=(game_engine, evaluation_function, best_score_list, cube),
         )
 
-        game_engine_thread.start()
-        print("Started")
-        game_engine_thread.join(
-            time_limit
-        )  # Wait for the thread to finish, but only for the given time limit
-        print("Finished")
+        processes.append(game_engine_process)
+        results.append(best_score_list)
+        names.append(f"{game_engine.__name__} - {evaluation_function.__name__}")
+
+    for process in processes:
+        process.start()
+    time.sleep(time_limit)
+    for process in processes:
+        process.terminate()
+    prepared_data = {}
+    for result, name in zip(results, names):
         # Plot score evolution with time
-        scores = [score[0] for score in best_score_list]
-        times = [
-            (score[2] - best_score_list[0][2]).total_seconds()
-            for score in best_score_list
-        ]
-        print(len(scores), len(times))
+        scores = [score[0] for score in result]
+        times = [(score[2] - result[-1][2]).total_seconds() for score in result]
+        prepared_data[name] = (scores, times)
+    return prepared_data
 
-        # Increase plot size
-        plt.figure(figsize=(10, 10))
-        plt.plot(times, scores)
-        plt.xlabel("Time (s)")
-        plt.ylabel("Score")
-        plt.title(f"Score evolution with time for {game_engine.__name__} and {name}")
-        plt.savefig(f"{game_engine.__name__}_{name}.png")
-        plt.clf()
 
-        results[f"{game_engine.__name__}_{name}"] = (scores, times)
+def compute_and_plot_evolution(all_configs):
+    prepared_data = compute_score_evolution_for_game_engine_and_evaluation_functions(
+        engine_eval_runs=all_configs
+    )
+    plt.clf()
+    plt.figure(figsize=(30, 30))
+    for name, (scores, times) in prepared_data.items():
+        normalized_scores = [score / scores[-1] for score in scores]
+        plt.plot(times, normalized_scores, label=name)
 
-    x += 1
-    if x == 4:
-        break
+    # increase figure size
 
-# Plot all results but normalize the scores and transform times to relative times in seconds
+    plt.xlabel("Time (s)")
+    plt.ylabel("Score")
+    plt.title(
+        f"Score evolution with time for all game engines and evaluation functions"
+    )
+    plt.legend()
+    plt.show()
 
-for name, (scores, times) in results.items():
-    normalized_scores = [score / scores[0] for score in scores]
-    relative_times = [(time - times[0]) for time in times]
 
-    plt.plot(relative_times, normalized_scores, label=name)
+def main():
+    all_configs = [
+        (game_engine, evaluation_function)
+        for game_engine in GAME_ENGINES
+        for evaluation_function in EVALUATION_FUNCTIONS.values()
+    ]
 
-plt.xlabel("Time (s)")
-plt.ylabel("Score")
-plt.title(f"Score evolution with time for all game engines and evaluation functions")
-plt.legend()
+    compute_and_plot_evolution(all_configs)
+
+
+if __name__ == "__main__":
+    main()
