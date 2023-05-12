d26f16faf35e124329ef996ba71bcf7eb2a4297a
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-05-12 10:48:54 +0200

    Adding benchmark capabilities

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from collections import defaultdict
 from datetime import datetime
 import time
 from typing import List
@@ -6,37 +7,49 @@ from typing import List
 from matplotlib import pyplot as plt
 
 from src.evaluation import EVALUATION_FUNCTIONS
+from src.evaluation.basic.combined import combined_simple_heuristics_evaluation_upscaled
 from src.modelisation.modelisation import Cube
 from src.search.engines import GAME_ENGINES
 from src.search.models.game_state import GameState
 
 
 # Wrapper evaluation function
-def wrapper_evaluation_function(evaluation_function, best_score_list: List):
+def wrapper_evaluation_function(
+    evaluation_function, best_score_list: List, reference_evaluation_function
+):
     def inner(*args, **kwargs):
         score = evaluation_function(*args, **kwargs)
+        reference_score = reference_evaluation_function(*args, **kwargs)
 
         # Check if score is better than the best score
-        if not best_score_list or score < best_score_list[0][0] * 0.9:
-            best_score_list.insert(0, (score, args[0].cube, datetime.now()))
+        if not best_score_list or reference_score < best_score_list[0][0] * 0.99:
+            best_score_list.insert(0, (reference_score, args[0].cube, datetime.now()))
 
         return score
 
     return inner
 
 
-def run_game_engine(game_engine, evaluation_function, best_score_list, cube):
+def run_game_engine(
+    game_engine,
+    evaluation_function,
+    best_score_list,
+    cube,
+    reference_evaluation_function,
+):
     instantiated_engine = game_engine(
         GameState(cube),
         evaluation_function=wrapper_evaluation_function(
-            evaluation_function, best_score_list
+            evaluation_function, best_score_list, reference_evaluation_function
         ),
     )
     instantiated_engine.run()
 
 
-def compute_score_evolution_for_game_engine_and_evaluation_functions(engine_eval_runs):
-    time_limit = 120  # Time limit in seconds
+def compute_score_evolution_for_game_engine_and_evaluation_functions(
+    engine_eval_runs, reference_evaluation_function
+):
+    time_limit = 10  # Time limit in seconds
     results = []
     processes = []
     names = []
@@ -60,7 +73,13 @@ def compute_score_evolution_for_game_engine_and_evaluation_functions(engine_eval
 
         game_engine_process = multiprocessing.Process(
             target=run_game_engine,
-            args=(game_engine, evaluation_function, best_score_list, cube),
+            args=(
+                game_engine,
+                evaluation_function,
+                best_score_list,
+                cube,
+                reference_evaluation_function,
+            ),
         )
 
         processes.append(game_engine_process)
@@ -69,7 +88,12 @@ def compute_score_evolution_for_game_engine_and_evaluation_functions(engine_eval
 
     for process in processes:
         process.start()
-    time.sleep(time_limit)
+
+    # Display progress bar (and sleep for time_limit seconds)
+    for i in range(time_limit):
+        print(f"Progress: {i+1}/{time_limit} seconds")
+        time.sleep(1)
+
     for process in processes:
         process.terminate()
     prepared_data = {}
@@ -83,13 +107,22 @@ def compute_score_evolution_for_game_engine_and_evaluation_functions(engine_eval
 
 def compute_and_plot_evolution(all_configs):
     prepared_data = compute_score_evolution_for_game_engine_and_evaluation_functions(
-        engine_eval_runs=all_configs
+        engine_eval_runs=all_configs,
+        reference_evaluation_function=combined_simple_heuristics_evaluation_upscaled,
     )
+
+    max_time = max(max(times) for scores, times in prepared_data.values())
+
     plt.clf()
-    plt.figure(figsize=(30, 30))
+    plt.figure(figsize=(15, 10))
+    print(max_time)
     for name, (scores, times) in prepared_data.items():
         normalized_scores = [score / scores[-1] for score in scores]
-        plt.plot(times, normalized_scores, label=name)
+        plt.plot(
+            [max_time, *times],
+            [normalized_scores[0], *normalized_scores],
+            label=name.replace("DijkstraGameEngine - ", ""),
+        )
 
     # increase figure size
 
@@ -98,9 +131,44 @@ def compute_and_plot_evolution(all_configs):
     plt.title(
         f"Score evolution with time for all game engines and evaluation functions"
     )
-    plt.legend()
+    plt.tight_layout(rect=[0, 0, 0.6, 1])
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
     plt.show()
 
+
+
+
+def plot_box_plots_of_best_scores(all_configs, number_of_eval=20):
+    best_scores = defaultdict(list)
+
+    names_to_index = {}
+
+    def get_index(name):
+        if name not in names_to_index:
+            names_to_index[name] = len(names_to_index)
+        return names_to_index[name]
+
+    for i in range(number_of_eval):
+        print(f"Run {i+1}/{number_of_eval}")
+        prepared_data = compute_score_evolution_for_game_engine_and_evaluation_functions(
+            engine_eval_runs=all_configs,
+            reference_evaluation_function=combined_simple_heuristics_evaluation_upscaled,
+        )
+
+        for name, (scores, times) in prepared_data.items():
+            normalized_scores = [score / scores[-1] for score in scores]
+            best_scores[get_index(name.replace("DijkstraGameEngine - ", ""))].append(normalized_scores[0])
+
+    plt.clf()
+    plt.xlabel("Game engine and evaluation function")
+    plt.ylabel("Best score")
+    plt.boxplot(best_scores.values(), labels=best_scores.keys())
+    plt.show()
+
+    # display table of names to index
+    print(names_to_index)
+    for name, index in names_to_index.items():
+        print(f"{index}: {name}")
 
 def main():
     all_configs = [
@@ -109,8 +177,9 @@ def main():
         for evaluation_function in EVALUATION_FUNCTIONS.values()
     ]
 
-    compute_and_plot_evolution(all_configs)
+    # compute_and_plot_evolution(all_configs)
 
+    plot_box_plots_of_best_scores(all_configs, number_of_eval=50)
 
 if __name__ == "__main__":
     main()
--- src/evaluation/basic/evaluation_functions.py
+++ src/evaluation/basic/evaluation_functions.py
@@ -1,7 +1,10 @@
 from .distance import distance_to_good_face_evaluation_function
 from .entropy import entropy_based_score_evaluation_function
 from .membership import face_color_membership_evaluation_function
-from .combined import combined_simple_heuristics_evaluation, combined_simple_heuristics_evaluation_upscaled
+from .combined import (
+    combined_simple_heuristics_evaluation,
+    combined_simple_heuristics_evaluation_upscaled,
+)
 
 BASIC_EVALUATION_FUNCTIONS = {
     "Distance to good face": distance_to_good_face_evaluation_function,
@@ -9,5 +12,4 @@ BASIC_EVALUATION_FUNCTIONS = {
     "Face color membership": face_color_membership_evaluation_function,
     "Combined simple heuristics": combined_simple_heuristics_evaluation,
     "Combined simple heuristics up-scaled": combined_simple_heuristics_evaluation_upscaled,
-
 }
